45c3da1862fc67e0e1d39e4576982b23f24bb129
inho <inho9377@naver.com>
2013-03-19 13:25:12 +0900

    exercise1.py

---
+++ Documents/exercise1.py
@@ -0,0 +1,19 @@
+
+
+
+
+def print_name(name) :
+	'''
+	this function print your name
+	'''
+	print 'your name is : \n', name
+
+
+def print_rename(name1, name2) :
+	print_name(name1)
+	print 'he is from', name2, 'family'
+
+
+
+
+print_rename('bruce','lee')
